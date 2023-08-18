53c63c5a1f9c7caa61815ff2e32e98626e4d9204
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-08-18 19:04:54 +0530

    Declare variables for error msg and email regex

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 "use strict";
 
-// Elements selection
+// DOM Elements selection
 const mainCard = document.querySelector(".container");
 const successCard = document.querySelector(".success-card");
 
+const errorMsg = document.querySelector("#error-msg");
 const emailInput = document.querySelector("#email-address");
 const buttonSignup = document.querySelector(".btn-signup");
 
 const emailSuccess = document.querySelector("#email-signed");
 const buttonDismiss = document.querySelector("#btn-dismiss");
+
+// Regex for email pattern matching
+const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
