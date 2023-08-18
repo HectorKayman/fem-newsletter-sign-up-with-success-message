8a913af1a44c4dd7219c4642b09809425db8a889
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-08-18 16:24:42 +0530

    Declare variables to grab DOM elements

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+// Elements selection
+const mainCard = document.querySelector(".container");
+const successCard = document.querySelector(".success-card");
+
+const emailInput = document.querySelector("#email-address");
+const buttonSignup = document.querySelector(".btn-signup");
+
+const emailSuccess = document.querySelector("#email-signed");
+const buttonDismiss = document.querySelector("#btn-dismiss");
