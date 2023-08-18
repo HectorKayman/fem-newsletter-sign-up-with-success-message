5eedec5bf1070f2e478604713ebc2dc737d052da
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2023-08-18 20:38:29 +0530

    Create event handler function for signup button
    Also add error handling logic for invalid email address

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ const buttonDismiss = document.querySelector("#btn-dismiss");
 
 // Regex for email pattern matching
 const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+// Variable to store user email
+const email = emailInput.value;
+
+// Event handler for Signup button
+buttonSignup.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Checks if email address is valid
+  const isValidEmail = emailPattern.test(email);
+  if (isValidEmail) {
+    console.log("Valid email address");
+  } else {
+    errorMsg.classList.remove("display-error");
+    emailInput.classList.add("error-box");
+    emailInput.style.cssText = "color: hsl(4, 100%, 60%)";
+  }
+});
